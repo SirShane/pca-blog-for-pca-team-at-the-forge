49fa4aed69c2212f16947e826f793058483d6363
Your Name <you@example.com>
2013-10-04 16:43:32 -0400

    Second Commit

---
+++ ShaneBlog.Web.Test.Unit/HomeControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using ShaneBlog.Web.Controllers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ShaneBlog.Web.Models;
+
+namespace ShaneBlog.Web.Tests.Unit
+{
+    public class HomeControllerTests
+    {
+
+        [Test]
+        public void IndexActionShouldDisplayPosts()
+        {
+            var controller = new HomeController();
+            var actual = controller.Index() as ViewResult;
+            Assert.That(actual.ViewName, Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public void IndexActionShouldReturnsPosts() {
+            var sut = new HomeController();
+            var action = sut.Index() as ViewResult;
+            Assert.That(action.ViewData.Model, Is.InstanceOf(typeof(IList<Post>)));
+        }
+
+        [Test]
+        public void IndexActionModelShouldNotBeEmpty() {
+            var sut = new HomeController();
+            var action = sut.Index() as ViewResult;
+            var posts = action.ViewData.Model as IList<Post>;
+            Assert.That(posts, Is.Not.Empty);
+        }
+
+        [Test]
+        public void PostShouldHaveABody() {
+            var sut = new Post();
+            // sut.Body = "";
+        }
+
+        [Test]
+        public void PostShouldHaveABodyOfBoo() {
+            var sut = new HomeController();
+            var action = sut.Index() as ViewResult;
+            var posts = action.ViewData.Model as IList<Post>;
+            var actual = posts[0];
+            Assert.That(actual.Body, Is.EqualTo("Boo"));
+        }
+    }
+}
---
+++ ShaneBlog.Web/Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using ShaneBlog.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShaneBlog.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var model = new List<Post>();
+            for (int i = 0; i < 5; i++)
+            {
+                model.Add(new Post() { Body = "Boo" });
+            }
+            return View("Index", model);
+        }
+
+        //
+        // GET: /Home/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
